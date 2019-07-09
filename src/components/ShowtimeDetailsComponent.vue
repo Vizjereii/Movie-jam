47c53e85f033bc82456d7ed0afa65035c2e95abe
Vizjereii <Parasite1551@gmail.com>
2019-07-09 19:46:23 +0300

    - card details mockup

--- src/components/ShowtimeDetailsComponent.vue
+++ src/components/ShowtimeDetailsComponent.vue
@@ -1,9 +1,19 @@
 <template>
   <div class="details-container">
+    <v-btn class="my-4" round>
+      <v-icon medium>info</v-icon>Movie Details
+    </v-btn>
+    <v-layout align-center justify-center row wrap pa-3 mb-2>
+      <v-flex v-for="time in getShowtimes" :key="time" xs3>
+        <v-card dark color="#81C784">
+          <v-card-text class="px-0 py-2">{{time}}</v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
     <v-card-title primary-title>
-      <h3 class="headline mb-0">{{showtimesTitle}}</h3>
+      <h3 class="headline mb-0 title-name">{{showtimesTitle}}</h3>
     </v-card-title>
-    <v-btn @click="test()">push</v-btn>
+    <v-btn @click="test()">test</v-btn>
   </div>
 </template>
 
@@ -47,5 +57,12 @@ export default {
   width: 100%;
   color: #fff;
   top: 0;
+}
+
+.title-name {
+  position: absolute;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
