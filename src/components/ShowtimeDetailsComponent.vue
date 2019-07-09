1cb3d2a96f0aef5a9de76d0b272b1ed6c0c043f2
Vizjereii <Parasite1551@gmail.com>
2019-07-09 17:02:37 +0300

    - card details showtimes not-so-random picker

--- src/components/ShowtimeDetailsComponent.vue
+++ src/components/ShowtimeDetailsComponent.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="even-height">
+  <div class="details-container">
     <v-card-title primary-title>
       <h3 class="headline mb-0">{{showtimesTitle}}</h3>
     </v-card-title>
+    <v-btn @click="test()">push</v-btn>
   </div>
 </template>
 
@@ -17,16 +18,34 @@ export default {
       type: Number,
       required: true
     }
+  },
+  data() {
+    return {
+      selectedDate: new Date().toISOString().substr(0, 10)
+    };
+  },
+  computed: {
+    getShowtimes() {
+      return this.$store.getters.getShowtimes(
+        this.showtimesId + this.selectedDate
+      );
+    }
+  },
+  methods: {
+    test() {
+      console.log(this.getShowtimes);
+      console.log(this.selectedDate);
+    }
   }
 };
 </script>
 
 <style scoped>
-.even-height {
+.details-container {
   position: absolute;
   height: 100%;
   width: 100%;
   color: #fff;
-  top: 0
+  top: 0;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,50 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import seedRandom from "seedrandom";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    activeMoviesList: []
+    activeMoviesList: [],
+    showtimesList: [
+      "10:15",
+      "10:55",
+      "11:35",
+      "12:15",
+      "12:50",
+      "13:30",
+      "14:10",
+      "14:50",
+      "15:25",
+      "16:05",
+      "16:45",
+      "17:25",
+      "18:00",
+      "18:20",
+      "18:40",
+      "19:20",
+      "20:00",
+      "20:35",
+      "21:15",
+      "21:55",
+      "22:35",
+      "23:10",
+      "23:50"
+    ]
   },
   mutations: {
     populateActiveMoviesList(state, payload) {
       state.activeMoviesList = payload;
+    }
+  },
+  getters: {
+    getShowtimes: state => seed => {
+      const rng = new seedRandom(seed);
+      return state.showtimesList.filter(() => {
+        if (Math.round(rng()) === 1) return true;
+      });
     }
   },
   actions: {
