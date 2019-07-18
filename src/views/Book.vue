a4d57752d4ff49378b51caba5d61e89ec7ad9f29
Vizjereii <Parasite1551@gmail.com>
2019-07-18 22:14:08 +0300

    - chairs drawing stub

--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,18 +1,35 @@
 <template>
-  <v-layout row justify-start class="even-height scrolling-wrapper booking-container">
-    <v-flex class="headline mb-0 font-weight-medium pa-2">
+  <v-layout row wrap justify-start class="even-height scrolling-wrapper booking-container">
+    <v-flex xs12 class="headline mb-0 font-weight-medium pa-2">
       <div>Booking tickets for "{{currentMovieData.title}}"</div>
       <div>on {{displayDate}}</div>
       <div>starts at {{timeslot}}</div>
     </v-flex>
+    <svg width="600" height="600">
+      <g v-for="(row, rowIndex) in chairRows" :key="row">
+        <path
+          v-for="(chairInRow, chairIndex) in chairsPerRow"
+          :key="chairInRow"
+          :transform="'translate(' + (1+chairIndex)*50 + ',' + (1+rowIndex)*50 + '.000000) scale(0.0035000,-0.005000)'"
+          fill="white"
+          :d="chairSvg"
+        />
+      </g>
+    </svg>
   </v-layout>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapState } from "vuex";
 import { format } from "date-fns";
 
 export default {
+  data() {
+    return {
+      chairRows: 11,
+      chairsPerRow: 8
+    };
+  },
   props: {
     movieId: {
       type: Number,
@@ -24,6 +41,7 @@ export default {
     }
   },
   computed: {
+    ...mapState(["chairSvg"]),
     ...mapGetters(["getMovieById"]),
     currentMovieData() {
       return this.getMovieById(this.movieId);
@@ -43,5 +61,11 @@ export default {
   width: 100%;
   color: #fff;
   font-family: "Roboto", sans-serif;
+}
+
+svg {
+  border: 4px #fff;
+  left: 0;
+  transform: translateX(50%);
 }
 </style>
