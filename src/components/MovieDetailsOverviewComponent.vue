9101e2709656c6a105ec96a7e6f05a4d0c9a8c18
DESKTOP-O1I0J0U\paras <parasite1551@gmail.com>
2021-04-06 20:52:43 +0200

    - a bit of styling and thinking on details overview component

--- src/components/MovieDetailsOverviewComponent.vue
+++ src/components/MovieDetailsOverviewComponent.vue
@@ -1,24 +1,22 @@
 ﻿<template>
   <div>
-    <div>{{ movieData.title }}</div>
-    <div>{{ movieData.tagline }}</div>
-    <div>{{ movieData.overview }}</div>
-    <div>Runtime: {{ movieData.runtime }}</div>
+    <v-card-title>{{ movieData.title }}</v-card-title>
+    <v-card-title class="pt-0 mb-0 mt-0 font-italic" v-if="movieData.tagline">"{{ movieData.tagline }}"</v-card-title>
+    <v-divider dark></v-divider>
+    <v-card-text class="text-xs-justify">{{ movieData.overview }}</v-card-text>
+    <v-flex class="d-flex justify-space-between mx-5">
+      <p class="text-xs-left" v-if="isOnDetailsPage">Release date: {{ movieData.release_date }}</p>
+      <p class="text-xs-right">Runtime: {{ getRuntimeFormatted }}</p>      
+    </v-flex>
     <template v-if="isOnDetailsPage">
       <div>Genres: {{ movieData.genres }}</div>
-      <div>production countries:
-        <div v-for="country in movieData.production_countries"
-             :key="country.id">
-          {{ country.name }}
-        </div>
-      </div>
+      <div>Country: {{ getCountryList }}</div>
       <div>production companies:
         <div v-for="company in movieData.production_companies"
              :key="company.id">
           {{ company.name }}
         </div>
-      </div>
-      <div>Release date: {{ movieData.release_date }}</div>      
+      </div>            
       <div>Videos: {{ movieData.videos }}</div>
       <div>Images: {{ movieData.images }}</div>
     </template>
@@ -37,6 +35,19 @@ export default {
   computed: {
     isOnDetailsPage() {      
       return this.$route.name === "details";
+    },
+    getRuntimeFormatted() {
+      const totalMinutes = this.movieData.runtime;
+      const hours = Math.floor(totalMinutes / 60);
+      const minutes = totalMinutes - hours * 60;
+      return `${hours}h ${minutes}m`
+    },
+    getCountryList() {
+      const countryList = this.movieData.production_countries;
+      console.log(countryList)
+      return countryList.reduce((acc, cur) => {
+        return acc + ', ' + cur.iso_3166_1;
+      }, countryList[0].iso_3166_1);
     }
   }
 }
